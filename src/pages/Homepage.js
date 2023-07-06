1dc50a7bac1dc302515f4da731bec1cdce7c271b
Mert KARACA <izm.mertkaraca@hotmail.com>
2023-07-06 07:06:30 +0300

    mapping

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -22,75 +22,9 @@ const Homepage = (props) => {
   const [lastMonthSpendings, setLastMonthSpendings] = useState([]);
   const [monthlySpendings, setMonthlySpendings] = useState([]);
 
-  const pieData = {
-    labels:
-      lastMonthSpendings &&
-      lastMonthSpendings.map((item) => {
-        return item.name;
-      }),
-    datasets: [
-      {
-        label: "# of Votes",
-        data:
-          lastMonthSpendings &&
-          lastMonthSpendings.map((item) => {
-            return item.amount;
-          }),
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  let pieData;
 
-  const barData = {
-    labels:
-      monthlySpendings &&
-      monthlySpendings.map((item) => {
-        return item.month;
-      }),
-    datasets: [
-      {
-        label: "Spend amount",
-        data:
-          monthlySpendings &&
-          monthlySpendings.map((item) => {
-            return item.amount;
-          }),
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  let barData;
 
   useEffect(() => {
     fetch(`http://localhost:8080/homepage/${username}`)
@@ -98,6 +32,68 @@ const Homepage = (props) => {
       .then((data) => {
         setMonthlySpendings(data.monthly);
         setLastMonthSpendings(data.lastMonth);
+      })
+      .then(() => {
+        pieData = {
+          labels: lastMonthSpendings.map((item) => {
+            return item.name;
+          }),
+          datasets: [
+            {
+              label: "# of Votes",
+              data: lastMonthSpendings.map((item) => {
+                return item.amount;
+              }),
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
+        barData = {
+          labels: monthlySpendings.map((item) => {
+            return item.month;
+          }),
+          datasets: [
+            {
+              label: "Spend amount",
+              data: monthlySpendings.map((item) => {
+                return item.amount;
+              }),
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
       });
   }, []);
 
@@ -125,9 +121,7 @@ const Homepage = (props) => {
             })}
           </div>
           <div className={classes.leftTable2}>
-            <Pie
-              data={pieData}
-            />
+            <Pie data={pieData} />
           </div>
         </div>
         <div className={classes.rightContainer}>
